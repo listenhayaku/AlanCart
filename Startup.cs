6f7273b14255c5695cc19980b31565d89ed9483e
listenhayaku <hhc8811105@ccit.ndu.edu.tw>
2026-02-02 16:08:45 +0800

    OK

---
+++ Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(AlanCart.Startup))]
+namespace AlanCart
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
